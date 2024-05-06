a09280c01f4f2d3dade517ccea5fe0cc0a0a37d3
“güray <kaleligiray@gmail.com>
2024-05-06 22:38:01 +0300

    user information

---
+++ Coding Practices/User_Information.py
@@ -0,0 +1,9 @@
+print("Get User Information and Present Them")
+
+name = input("User name: ")
+surname = input("User surname: ")
+phone = int(input("Phone Number: "))
+
+print(f"Name: {name}")
+print(f"Surname: {surname}")
+print(f"Phone Number: {phone}")
